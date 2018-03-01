802cf95d741593ce95a645e596fe4264bb36a706
Zac Willmington <zac.willmington@gmail.com>
2018-03-01 23:56:15 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,7 @@ class Scraper
   end
 
   def get_page
-      html = open("https://flatironschool.com/")
+      html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
       doc = Nokogiri::HTML(html)
       doc
     end
