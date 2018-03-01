4aa72dbb67e289153a27a96505d05892cdecb353
Zac Willmington <zac.willmington@gmail.com>
2018-03-01 23:53:42 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
+
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +17,25 @@ class Scraper
       end
     end
   end
-  
+
+  def get_page
+      html = open("https://flatironschool.com/")
+      doc = Nokogiri::HTML(html)
+      doc
+    end
+
+  def get_courses
+      self.get_page.css(".posts-holder") #or #".post".text
+  end
+
+  def make_courses
+      self.get_courses.each do |course|
+          binding.pry
+
+          course = Course.new
+          #binding.pry
+      end
+  end
+
+
 end
-
-
-
