3045bdef3c354e3c791e6a0703f29e5958d28e21
Zac Willmington <zac.willmington@gmail.com>
2018-03-02 00:25:28 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,8 +22,9 @@ class Scraper
             #binding.pry
             self.get_courses.collect do |course|
             #binding.pry
-             course = Course.new
              course.css("h2").text
+             course = Course.new
+
              #binding.pry
         end
         #binding.pry
