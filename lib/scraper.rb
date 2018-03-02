6c4d4aad63f9f1a3fea85520f10eb59c18604c86
Zac Willmington <zac.willmington@gmail.com>
2018-03-02 00:06:51 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -34,8 +34,7 @@ class Scraper
           self.get_courses.each do |course|
           #binding.pry
            course = Course.new
-           course
-          #binding.pry
+           #binding.pry
       end
   end
 
