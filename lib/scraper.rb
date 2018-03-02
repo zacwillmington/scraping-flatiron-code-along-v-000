7889e1bb648a27b72ee9b33bad128543ecad3e8f
Zac Willmington <zac.willmington@gmail.com>
2018-03-02 00:11:00 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ require_relative './course.rb'
 
 class Scraper
 
-    attr_accessor :get_page, :get_courses, :make_courses, :print_courses
+    reader_accessor :get_page, :get_courses, :make_courses, :print_courses
 
     def get_page
         html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
